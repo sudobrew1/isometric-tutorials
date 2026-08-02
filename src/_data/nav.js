3e58bf629dd37d48909620b1ceaa7fbf6825ced7
sudobrew <me@sudobrew.net>
2026-08-02 02:54:22 -0600

    Adding and finishing Texture Painting page for Blender

--- src/_data/nav.js
+++ src/_data/nav.js
@@ -6,7 +6,8 @@ module.exports = [
       { label: "Installing", url: "/blender/installing/" },
       { label: "Modeling Tiles", url: "/blender/modeling-tiles/" },
       { label: "UV Mapping Tile Textures", url: "/blender/uv-mapping-tiles/" },
-      { label: "Blender Templates", url: "/blender/blender-templates/" },
+      { label: "Texture Painting Tiles", url: "/blender/texture-painting-tiles/" },
+      { label: "Blender Templates", url: "/blender/blender-templates/" }
     ],
   },
   {
